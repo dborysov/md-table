7038d9708c062eca99e11bad9b088d2aa501baa5
Dmytro Borysov <4720760+dborysov@users.noreply.github.com>
2022-09-23 14:55:27 +0200

    fix: trimLeft

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-type TrimLeft<TInput extends string> = TInput extends ` ${infer Value}` ? TrimRight<Value> : TInput;
+type TrimLeft<TInput extends string> = TInput extends ` ${infer Value}` ? TrimLeft<Value> : TInput;
 
 type TrimRight<TInput extends string> = TInput extends `${infer Value} ` ? TrimRight<Value> : TInput;
 
