9f72f6755322e06af5849ee6f71a186779cf152c
Dmytro Borysov <4720760+dborysov@users.noreply.github.com>
2023-01-10 15:55:43 +0100

    chore: minify source code

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config = (): webpack.Configuration => ({
+  mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+});
+
+export default config;
